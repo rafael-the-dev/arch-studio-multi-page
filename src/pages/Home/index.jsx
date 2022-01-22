5cb17eb0818a5279f4aba7617836bdc20ea3368f
Rafael Tivane <luislangabusiness@gmail.com>
2022-01-22 09:31:39 +0200

    adding customs indicators to carousel

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,6 +4,9 @@ import classNames from 'classnames';
 import { useBackground, useDisplay, useResponsive, useTypography } from '../../styles';
 import { useStyles } from './styles'
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import { useCallback, useEffect, useRef } from 'react'
 
 const Home = () => {
     const bg = useBackground();
@@ -12,25 +15,68 @@ const Home = () => {
     const responsive = useResponsive();
     const text = useTypography();
 
+    const CarouselItem = useCallback(() => (
+        <section className={classNames(display.flex, display.flexColumn, classes.px, classes.hero, 
+            classes.paramourHero, bg.center, bg.noRepeat, bg.cover, display.justifyCenter, display.alignStart)}>
+            <Typography component="h1" variant="h3" className={classNames(text.font7, text.textLight, text.alignLeft)}>
+                Project<br/>paramour
+            </Typography>
+            <Typography  className={classNames(display.mt1, text.textLight, text.rem9, classes.heroDescription, text.alignLeft)}>
+                Project made for an art museum near Southwest London. Project Paramour is a statement of bold, modern architecture.
+            </Typography>
+            <Link to="/portfolio" className={classNames(text.noUnderline, display.mt2)}>
+                <Button 
+                    endIcon={<ArrowForwardIcon />}
+                    variant="contained"
+                    className={classNames(classes.heroButton)}>
+                    See our portfolio
+                </Button>
+            </Link>
+        </section>
+    ), [ bg, classes, display, text ]);
+
+    const CustomIndicator = useCallback(() => (
+
+        <div className={classNames(display.flex, classes.carouselButtonsContainer, display.absolute)}>
+            <button className={classNames(display.borderNone, display.outlineNone, classes.carouselButton)}>1</button>
+            <button className={classNames(display.borderNone, display.outlineNone, classes.carouselButton)}>2</button>
+            <button className={classNames(display.borderNone, display.outlineNone, classes.carouselButton)}>3</button>
+            <button className={classNames(display.borderNone, display.outlineNone, classes.carouselButton)}>4</button>
+        </div>
+    ), [ classes, display]);
+    
+    const indicatorRender = useCallback((func, isSelected, item) => {
+        return (
+            <button 
+                onClick={func} 
+                className={classNames(display.borderNone, display.outlineNone, classes.carouselButton, 
+                {[classes.selectedIndicator]: isSelected, [text.textLight]: isSelected})}>
+                { item + 1}
+            </button>
+        );
+    }, [ classes, display, text ]);
+
+
     return (
-        <main>
-            <section className={classNames(display.flex, display.flexColumn, classes.px, classes.hero, 
-                classes.paramourHero, bg.center, bg.noRepeat, bg.cover, display.justifyCenter)}>
-                <Typography component="h1" variant="h3" className={classNames(text.font7, text.textLight)}>
-                    Project<br/>paramour
-                </Typography>
-                <Typography  className={classNames(display.mt1, text.textLight, text.rem9)}>
-                    Project made for an art museum near Southwest London. Project Paramour is a statement of bold, modern architecture.
-                </Typography>
-                <Link to="/portfolio" className={classNames(text.noUnderline, display.mt2)}>
-                    <Button 
-                        endIcon={<ArrowForwardIcon />}
-                        variant="contained"
-                        className={classNames(classes.heroButton)}>
-                        See our portfolio
-                    </Button>
-                </Link>
-            </section>
+        <main className={classNames(classes.main)}>
+            <div className={classNames(display.relative)}>
+                <Carousel 
+                    showArrows={false} 
+                    showIndicators={true} 
+                    showThumbs={false}
+                    renderIndicator={indicatorRender}>
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+                {/*<div className={classNames(display.flex, classes.carouselButtonsContainer, display.absolute)}>
+                    <button className={classNames(display.borderNone, display.outlineNone, classes.carouselButton)}>1</button>
+                    <button className={classNames(display.borderNone, display.outlineNone, classes.carouselButton)}>2</button>
+                    <button className={classNames(display.borderNone, display.outlineNone, classes.carouselButton)}>3</button>
+                    <button className={classNames(display.borderNone, display.outlineNone, classes.carouselButton)}>4</button>
+                </div>*/}
+            </div>
         </main>
     )
 };
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,24 +1,58 @@
 import { makeStyles } from "@mui/styles";
 import paramourImage from '../../assets/images/home/mobile/image-hero-paramour.jpg'
+import paramourTabletImage from '../../assets/images/home/tablet/image-hero-paramour.jpg'
+import paramourDesktopImage from '../../assets/images/home/desktop/image-hero-paramour.jpg'
 const linearGradient = 'linear-gradient(to bottom, rgba(0, 0, 0, .4), rgba(0, 0, 0, .1))'
 export const useStyles = makeStyles(theme => ({
-    px: {
-        paddingLeft: '5%',
-        paddingRight: '5%',
+    main: {
+        [theme.breakpoints.up('sm')]: {
+            paddingLeft: '5%',
+            paddingRight: '5%',
+        },
         [theme.breakpoints.up(1000)]: {
             paddingLeft: '10%',
             paddingRight: '10%',
         }
     },
+    px: {
+        paddingLeft: '5%',
+        paddingRight: '5%',
+    },
     hero: {
         height: 500
     },
     paramourHero: {
-        backgroundImage: `${linearGradient}, url(${paramourImage})`
+        backgroundImage: `${linearGradient}, url(${paramourImage})`,
+        [theme.breakpoints.up('sm')]: {
+            backgroundImage: `${linearGradient}, url(${paramourTabletImage})`,
+        },
+        [theme.breakpoints.up('md')]: {
+            backgroundImage: `${linearGradient}, url(${paramourDesktopImage})`,
+        }
+    },
+    heroDescription: {
+        [theme.breakpoints.up(700)]: {
+            maxWidth: '60%'
+        }
     },
     heroButton: {
         backgroundColor: '#000 !important',
         padding: '10px 16px !important',
         textTransform: 'capitalize !important'
+    },
+    carouselButtonsContainer: {
+        bottom: -10,//20,
+        left: -26,
+        [theme.breakpoints.up('sm')]: {
+            left: -26
+        }
+    },
+    carouselButton: {
+        backgroundColor: '#FFF',
+        height: 40,
+        width: 40
+    },
+    selectedIndicator: {
+        backgroundColor: '#1B1D23 !important'
     }
 }));
